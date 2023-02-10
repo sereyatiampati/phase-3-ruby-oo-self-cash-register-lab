a04e8b4d2d6f3ee9ac973444ed048ddcbf08d816
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-02-11 00:39:52 +0300

    almost done

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+require 'pry'
+class CashRegister
+    attr_accessor :total, :quantity
+    attr_reader :discount
 
+    def initialize(discount=0, total = 0)
+        @total=total
+        @discount=discount
+    end
+
+    def add_item(title, price, quantity=0)
+        self.total += price
+    end
+
+    def apply_discount
+        total = total - total * self.discount/100
+        "Discounted"
+        total
+    end
+end
+#binding.pry
